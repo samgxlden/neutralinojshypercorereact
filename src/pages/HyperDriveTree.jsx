4b2d3895f48a146e1db72433d3bb6a3e4216cd5f
samgxlden <samgxlden@gmail.com>
2022-12-05 00:32:29 +0000

    fix: generate srr prerender

--- src/pages/HyperDriveTree.jsx
+++ src/pages/HyperDriveTree.jsx
@@ -5,10 +5,8 @@
 
 import React from "react";
 import HyperDrive2 from "../components/hyperdrive/HyperDrive2";
-import HyperDriveTreePage from "../components/hyperdrive/HyperDriveTreePage";
 
 export default function HyperDriveTree() {
 
-  //return (<HyperDriveTreePage/>)
   return (<HyperDrive2/>)
 }
